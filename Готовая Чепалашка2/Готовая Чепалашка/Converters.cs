46a9d745b6f4dd6d9a9707164f2f89bdd490bb49
Бибыч <vaprosav2@yandex.ru>
2025-05-17 19:46:20 +0400

    Add files via upload

---
+++ Готовая Чепалашка2/Готовая Чепалашка/Converters.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Proga1
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isVisible)
+            {
+                return isVisible ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibility)
+            {
+                return visibility == Visibility.Visible;
+            }
+            return false;
+        }
+    }
+
+    public class BoolToLoadingTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isLoading)
+            {
+                return isLoading ? "Загрузка данных..." : "";
+            }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString() == "Загрузка данных...";
+        }
+    }
+}
